e7abd8ed43802c3f1a8a80130cd81b9a8b5c1c8d
Dariel Vasquez <darielalejandro10@gmail.com>
2023-08-03 12:34:54 -0600

    added security group

---
+++ security_group.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "SecurityGroup" {
+  name = "SecurityGroup"
+  description = "Security group for Auto Scaling Group instances"
+  vpc_id = aws_vpc.ChallengeVPC.id
+
+  ingress {
+    description = "Allow SSH access from any IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic from any IP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic from any IP"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SecurityGroup"
+  }
+}
